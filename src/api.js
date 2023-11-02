10aa34075ddb49b1d6c85e10451d75f591bc4693
DianaRoman17 <127916707+Gudari17@users.noreply.github.com>
2023-11-02 03:03:13 -0500

    added files, functionality in java and restyled weather forecast

--- src/api.js
+++ src/api.js
@@ -60,4 +60,32 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">
+  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
+              alt=""
+              width="36"
+            /></div>
+            <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature-max"> 18 </div>
+            <div class="weather-forecast-temperature-min"> 12 </div>
+          </div>
+        </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 searchCity("Chicago");
+
+displayForecast();
